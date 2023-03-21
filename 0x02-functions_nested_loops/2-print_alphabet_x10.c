6d12653b634f802c723e24fc6a06dbcdac7322a8
yassine aassila <yassineassila321@gmail.com>
2023-03-21 15:14:50 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -17,13 +17,3 @@ void print_alphabet_x10(void)
 		putchar('\n');
 	}
 }
-/**
- * main - The main function of the program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
