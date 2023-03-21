9d90f1a99495e9f412fcc32fdea2459633f424cb
yassine aassila <yassineassila321@gmail.com>
2023-03-21 15:28:41 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
  *                       by a new line.
  */
 void print_alphabet_x10(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+	int j;
+	for (j = 1; i <= 10; j++)
 	{
-		char letter = 'a';
-
-		while (letter <= 'z')
-		{
-			putchar(letter);
-			letter++;
-		}
-		putchar('\n');
+		_putchar(j);
 	}
+
+	_putchar('\n');
 }
