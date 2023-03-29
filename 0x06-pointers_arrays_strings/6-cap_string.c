275b66c4872566460613f68f9418a3a4282fbba3
yassine aassila <yassineassila321@gmail.com>
2023-03-29 13:07:18 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes all words of a string
+ *
+ * @s: The string to be modified
+ *
+ * Return: A pointer to the modified string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+            s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+            s[i] == '!' || s[i] == '?' || s[i] == '"' ||
+            s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}') {
+            if (s[i + 1] >= 'a' && s[i + 1] <= 'z') {
+                s[i + 1] = s[i + 1] - 'a' + 'A';
+            }
+        }
+    }
+
+    return s;
+}
